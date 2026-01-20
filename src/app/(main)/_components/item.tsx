2c5e5c26e334fa5bd356b84e61e25d9a30dce3d1
HaveNiceDa <2676424029@qq.com>
2026-01-20 20:00:40 +0800

    '侧边栏

---
+++ src/app/(main)/_components/item.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  ChevronDown,
+  ChevronRight,
+  LucideIcon,
+  MoreHorizontal,
+  Plus,
+  Trash,
+} from "lucide-react";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useUser } from "@clerk/clerk-react";
+
+import { cn } from "@/lib/utils";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+interface ItemProps {
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
+  label: string;
+  onClick?: () => void;
+  icon: LucideIcon;
+}
+
+export const Item = ({
+  label,
+  onClick,
+  icon: Icon,
+  active,
+  documentIcon,
+  isSearch,
+  level = 0,
+  onExpand,
+  expanded,
+}: ItemProps) => {
+  return (
+    <div
+      onClick={onClick}
+      role="button"
+      style={{
+        paddingLeft: level ? `${level * 12 + 12}px` : "12px",
+      }}
+      className={cn(
+        "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        active && "bg-primary/5 text-primary",
+      )}
+    ></div>
+  );
+};
